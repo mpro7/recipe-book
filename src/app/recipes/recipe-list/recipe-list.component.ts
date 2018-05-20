d4c0748381b35654e4d445d5fe6387da3bba6a7e
mpro7 <justinstarlight@gmail.com>
2018-05-20 10:52:34 +0200

    added recipe model and markup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  public recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'A Test Description',
+    'https://cdn.pixabay.com/photo/2012/06/08/16/09/heart-49576_960_720.jpg')
+  ];
 
   constructor() { }
 
